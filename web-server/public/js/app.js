c0259a1962fb6bafb50337bf75d2202639c7d3fb
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-01-10 21:37:40 +0300

    Fetch weather forcast & display resutl on the ui

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,20 +1,71 @@
-const getCityUpdateInfo = require('./utils/forcast');
-const city = process.argv[2];
+const searchCityForm = document.querySelector('form');
+const cityDetailsInfo = document.querySelector('.details');
+const card = document.querySelector('.card');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
-if (!city) {
-  console.log('Please provide city name!');
-  return;
+const render = (data) => {
+  // updating weather details
+  const { locationInfo: cityDetails, weatherInfo } = data;
+  const html = `
+      <h5 class="my-3">
+      <!-- City Name -->
+        ${cityDetails.EnglishName}
+      </h5>
+      <div class="my-3">
+        <!-- Weather Conditions -->
+        ${weatherInfo.WeatherText}
+      </div>
+      <div class="display-4 my-4">
+        <span>${weatherInfo.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+      </div>
+  `;
+  cityDetailsInfo.innerHTML = html;
+
+  // setting day and night and icon images
+  const imgSrc = weatherInfo.IsDayTime ? '../img/day.svg' : '../img/night.svg';
+  time.setAttribute('src', imgSrc);
+
+  const iconSrc = `../img/icons/${weatherInfo.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+};
+
+searchCityForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const city = searchCityForm.city.value.trim();
+  searchCityForm.reset();
+  console.log(city);
+
+  // update the ui with the city
+  fetch('http://localhost:3000/weather?city=kampala')
+    .then((response) => {
+      response
+        .json()
+        .then((data) => {
+          render(data);
+        })
+        .catch((err) => console.log(err)); // eslint-disable-line no-console;
+    })
+    .catch((err) => console.log(err)); // eslint-disable-line no-console;
+  // set Local storage
+  localStorage.setItem('city', city);
+});
+
+if (localStorage.getItem('city')) {
+  fetch('http://localhost:3000/weather?city=kampala')
+    .then((response) => {
+      response
+        .json()
+        .then((data) => {
+          render(data);
+        })
+        .catch((err) => console.log(err)); // eslint-disable-line no-console;
+    })
+    .catch((err) => console.log(err)); // eslint-disable-line no-console;
 }
-
-getCityUpdateInfo(city)
-  .then((data) => {
-    const { locationInfo, weatherInfo } = data;
-    console.log(locationInfo, weatherInfo);
-    const info = `Current temperature Info:
-        City Name: ${locationInfo.EnglishName}
-        Celcius: ${weatherInfo.Temperature.Metric.Value}
-        Fahrenheit: ${weatherInfo.Temperature.Imperial.Value}
-        `;
-    console.log(info);
-  })
-  .catch((err) => console.log('Unable to fetch weather information'));
